d8f15964035b586ade81ccaf8a8daf49d0ba61a7
D3 <D3@H1-173noMacBook-Pro.local>
2019-07-03 18:23:09 +0900

    test vue

---
+++ web/vue/my-vue-app/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  <div id="dapp">
+    <button v-on:click="onClick">Clickable</button>
+  </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'dApp',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    onClick: function(event){
+      alert(event.target)
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
---
+++ web/vue/my-vue-app/src/Title.js
@@ -0,0 +1,24 @@
+
+// var title = new Vue({
+//     // // el: '#apppp',
+
+//     // data: {
+//     //     message: 'Vue.js'
+//     // },
+//     // computed: {
+//     //     computedMessage: function(){
+//     //         return this.message + '+'
+//     //     }
+//     // },
+//     // created: function(){
+
+//     // },
+//     // methods : {
+//     //     move: function(){
+
+//     //     },
+//     //     attack: function(){
+
+//     //     }
+//     // },
+// })
