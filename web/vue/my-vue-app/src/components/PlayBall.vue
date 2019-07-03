abf6d8bc639d82261bd4544d3b4d61ef6b4e808e
D3 <D3@H1-173noMacBook-Pro.local>
2019-07-03 18:50:02 +0900

    vue + TypeScript

---
+++ web/vue/my-vue-app/src/components/PlayBall.vue
@@ -0,0 +1,20 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+
+// Vue + TypeScript。https://jp.vuejs.org/v2/guide/typescript.html
+const TypeInferenceComponent_型推論版 = Vue.extend({
+    //  TypeScript の型推論を有効にする。＝＞　Vue.extend({〜〜})
+})
+  
+const Component_非型 = {
+    // これは型推論を持っていません、
+    // なぜなら、これは Vue コンポーネントのオプションであるということを伝えることができないためです。
+}
+</script>
