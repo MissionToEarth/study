989b2e04c134ab1fcb1e37b4c417e0ad82739eee
D3 <D3@H1-173noMacBook-Pro.local>
2019-08-20 14:46:25 +0900

    キーボード入力の取得をしてみた。

--- web/vue/my-vue-use_cdn/main.js
+++ web/vue/my-vue-use_cdn/main.js
@@ -9,7 +9,31 @@ var first_root_app = new Vue ({
         toggle: true,
         message: "init",
         TAパラ切り替えスイッチ: true,   //ソースコードにUTF-8が使えるんだぜ。使うぜ。先頭文字にascii文字を付けるのは命名規則としてありかもしれない。
+        key_input: "k-e-y",
     },
+
+    //監視するぞい
+    watch:{
+        toggle: function(newvalue, oldvalue) {
+            console.log(newvalue.toString() + " " + oldvalue.toString())
+        }
+    },
+
+    mounted() {
+
+        document.onkeydown = e => {
+    
+            this.key_input = e.key.toString();
+
+          // cmd + s は 動く
+          if (e.key === 'S' && e.shiftKey) {
+            
+            alert("キーダウン")
+            
+            return false
+          }
+        }
+    },    
 
     // 関数定義
     methods: {
@@ -19,6 +43,14 @@ var first_root_app = new Vue ({
         onButton: function(event){
             console.log("button down")
             this.toggle = !this.toggle
+        },
+        onPageDown: function(event){
+            console.error(JSON.stringify(event,null,"\t"))
+            alert("")
+        },
+        doSomething: function(event){
+            alert("do something")
+            this.key_input="do"
         }
     }
 })
